fa457cdcc67ddb122beca1459a0ddfca62af2d9d
gdbarros94 <gdbarros95@gmail.com>
2024-02-16 16:48:53 -0300

    aula sobre arrays e funcoes

---
+++ index.js
@@ -0,0 +1,49 @@
+// silence is golden
+
+/* var nome = prompt("Escreva seu nome");
+var cor = prompt("Escreva sua cor preferida");
+
+var frase = "A cor favorita de " + nome + " é " + cor + ".";
+
+console.log(frase);
+
+// exercicio 2
+var frase2 = prompt("Escreva uma frase");
+console.log(frase2.toUpperCase());
+console.log(frase2.replaceAll("o", "i"));
+console.log(frase2.length);
+ */
+
+//exercicio 3
+
+//var cachorros = ["raca 1", "raca 2", "raca 3", "raca 4", "raca 5"];
+//var escolha = prompt("Escolha um numero de 0 a 4");
+//console.log(cachorros[escolha]);
+
+//console.log(cachorros[prompt("digita 0 a 4")]);
+
+//splice
+
+//var array = ["a", "b", "c", "d", "e", "f", "g", "h"];
+//          [ 0,   1,   2,   3,   4,   5,   6,   7 ];
+//array.splice(3, 3)
+//console.log(array);
+
+
+//exercicio 4
+
+/* var array = [1,2,3,4,5,6];
+console.log(array.length);
+array.push(7);
+array.splice(3, 2);
+console.log(array.length); */
+
+// funcoes exercicio 2
+
+function escreveNome(nome){
+    console.log(`Ola ${nome}`);
+}
+
+escreveNome("Gabriel");
+escreveNome("Joao");
+escreveNome("maria");
